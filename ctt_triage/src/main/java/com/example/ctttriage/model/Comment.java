b18b260325b528734dd381fad28ac3e91e643f7c
Augustas Turka <augustasturka@gmail.com>
2026-04-29 18:32:49 +0300

    add lombok dep, use to generate model constructors

--- ctt_triage/src/main/java/com/example/ctttriage/model/Comment.java
+++ ctt_triage/src/main/java/com/example/ctttriage/model/Comment.java
@@ -2,8 +2,10 @@ package com.example.ctttriage.model;
 
 import jakarta.persistence.*;
 import java.util.UUID;
+import lombok.*;
 
-
+@Data
+@NoArgsConstructor
 @Entity
 @Table(name = "comments")
 public class Comment {
--- ctt_triage/src/main/java/com/example/ctttriage/model/Ticket.java
+++ ctt_triage/src/main/java/com/example/ctttriage/model/Ticket.java
@@ -2,7 +2,10 @@ package com.example.ctttriage.model;
 
 import jakarta.persistence.*;
 import java.util.UUID;
+import lombok.*;
 
+@Data
+@NoArgsConstructor
 @Entity
 @Table(name = "tickets")
 public class Ticket {
