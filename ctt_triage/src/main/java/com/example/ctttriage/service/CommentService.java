1dff389a525126bb6a8e00ace1ba128057212a00
Augustas Turka <augustasturka@gmail.com>
2026-05-03 11:49:47 +0300

    sketch main workflow of application

--- ctt_triage/src/main/java/com/example/ctttriage/service/CommentService.java
+++ ctt_triage/src/main/java/com/example/ctttriage/service/CommentService.java
@@ -1,13 +1,19 @@
 package com.example.ctttriage.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
 import io.swagger.v3.core.util.Json;
 
 import com.example.ctttriage.repositories.CommentRepository;
+import com.example.ctttriage.repositories.TicketRepository;
+import com.google.gson.JsonParser;
 import com.example.ctttriage.model.*;//all for now
 import com.example.ctttriage.dto.*;//all objects for now
 import com.example.ctttriage.dto.external.ExternalTicketData;
@@ -21,19 +27,43 @@ import lombok.RequiredArgsConstructor;
 public class CommentService {
 
     private final CommentRepository commentRepository;
+    private final TicketRepository ticketRepository;
 
     public List<CommentResponse> getAllComments () {
 
         return commentRepository.findAll().stream().map(this::mapCommentToCommentResponse).toList();
     }
 
-    // public List<Ticket> createTickets(Comment comment) {
-    //     //api call, give comment, get json
+    //TODO: update return type
+    public void createTickets(Comment comment) {
+        if(shouldCommentBecomeTicket(comment)) {//prompt something like "does this comment contain something that should become a technical ticket", no matter how many tickets
+            List<ExternalTicketData> tickets = buildTicketList(comment);
+            tickets.forEach(ticket -> {
+                Ticket entity = mapExternalTicketDataToTicket(ticket);
+                ticketRepository.save(entity);
+        });
+        };
+    }
 
-    //     //response? if no ticket, return, if yes- get json
+//helper methods
 
-    //     //mapping call, iterate over json and save each ticket
-    // }
+    private boolean shouldCommentBecomeTicket (Comment comment) {
+        //api call to decide if comment should become a ticket.
+        return true;
+    }
+
+    private List<ExternalTicketData> buildTicketList(Comment comment) {
+
+    String apiResponse = "api response";//TODO: placeholder
+    JsonArray jsonArray = JsonParser.parseString(apiResponse).getAsJsonArray();
+
+    List<ExternalTicketData> tickets = new ArrayList<>();
+    for (JsonElement element : jsonArray) {
+        JsonObject json = element.getAsJsonObject();
+        tickets.add(mapJsonResponseToExternalTicketData(json));
+    }
+    return tickets;
+}
 
 //mapping methods
 
@@ -55,5 +85,16 @@ public class CommentService {
 
         return response;
     }
+
+    private Ticket mapExternalTicketDataToTicket(ExternalTicketData data) {
+
+        Ticket ticket = new Ticket();
+        ticket.setTitle(data.getTitle());
+        ticket.setSummary(data.getSummary());
+        ticket.setCategory(data.getCategory());
+        ticket.setPriority(data.getPriority());
+
+        return ticket;
+}
     
 }
